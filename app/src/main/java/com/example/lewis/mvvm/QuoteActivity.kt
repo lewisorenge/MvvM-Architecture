af4b7603f3ba745d36a153217ffa806320338087
lewisorenge <lewisorenge@gmail.com>
2018-10-08 16:08:59 +0300

    initial workings

---
+++ app/src/main/java/com/example/lewis/mvvm/QuoteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lewis.mvvm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class QuoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quote)
+    }
+}
